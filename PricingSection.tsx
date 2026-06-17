57578723d66dc1d6fcb1d0ad7fc4a4f60f78e657
surendrakyadav2017 <surendrakyadav2017@gmail.com>
2026-06-17 19:59:32 +0530

    Update PricingSection.tsx

--- PricingSection.tsx
+++ PricingSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PricingPlan } from '../types';
+import { PricingPlan } from './types';
 import LucideIcon from './LucideIcon';
 
 interface PricingSectionProps {
